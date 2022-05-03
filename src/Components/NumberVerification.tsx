1a3325d85e375e04d16a911dd1e0049bf6878b0b
RIMPA DAS <--global>
2022-05-03 11:21:17 +0530

    number verification- done

--- src/Components/NumberVerification.tsx
+++ src/Components/NumberVerification.tsx
@@ -3,14 +3,19 @@ import * as yup from 'yup';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from 'firebase/auth';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { NumberVerificationLink } from 'Interface/numberVerification.interface';
 import AuthHeader from './AuthHeader';
 import TextInputField from './TextInputField';
 import Button from './Button';
 import FormErrorMessage from './FormErrorMessage';
+import { useNavigate } from 'react-router-dom';
+import { routes } from 'routes';
 
 export default function NumberVerification() {
+  const navigate = useNavigate();
   const numberVerificationSchema = yup.object().shape({
     phoneNumber: yup.string().trim().required('Phone number is required.'),
   });
@@ -24,17 +29,20 @@ export default function NumberVerification() {
   });
   const auth = getAuth();
   const [show, setShow] = useState(false);
+  const [hide, setHide] = useState(true);
   const [results, setResults] = useState<any>('');
 
-  const onSubmit = async (value: NumberVerificationLink) => {
+  const numberVerify = async (value: NumberVerificationLink) => {
     const verify = new RecaptchaVerifier('captcha-container', {}, auth);
     signInWithPhoneNumber(auth, value.phoneNumber, verify)
       .then((res: any) => {
         setResults(res);
         setShow(true);
+        setHide(false);
+        toast.success('otp sent');
       })
       .catch((err) => {
-        console.log(err);
+        toast.error(err.message);
       });
   };
 
@@ -44,10 +52,12 @@ export default function NumberVerification() {
       .then((res: any) => {
         const user = res.user;
         console.log(user);
+        toast.success('Verify successfully');
+        navigate(routes.listScreen);
       })
       .catch((err: any) => {
-        alert('Wrong code');
-        console.log(err);
+        toast.error('wrong otp');
+        toast.error(err.message);
       });
   };
 
@@ -56,32 +66,35 @@ export default function NumberVerification() {
       <div>
         <AuthHeader />
       </div>
-      <div className='min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
-        <div className='max-w-md w-full space-y-8'>
-          <div className='text-center'>
-            <h2 className='text-3xl font-extrabold text-gray-900'>Verify Your Number</h2>
-            <p className='mt-2'>Please enter your phone number</p>
-          </div>
-          <form className='mt-8 space-y-4' onSubmit={handleSubmit(onSubmit)}>
-            <div className='rounded-md -space-y-px'>
-              <div>
-                <TextInputField type='text' placeholder='Phone number' register={register('phoneNumber')} />
+      <ToastContainer />
+      {hide && (
+        <div className='min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
+          <div className='max-w-md w-full space-y-8'>
+            <div className='text-center'>
+              <h2 className='text-3xl font-extrabold text-gray-900'>Verify Your Number</h2>
+              <p className='mt-2'>Please enter your phone number</p>
+            </div>
+            <form className='mt-8 space-y-4' onSubmit={handleSubmit(numberVerify)}>
+              <div className='rounded-md -space-y-px'>
+                <div>
+                  <TextInputField type='text' placeholder='Phone number' register={register('phoneNumber')} />
+                </div>
+                <div id='captcha-container' className='pt-4'></div>
               </div>
-              <div id='captcha-container' className='pt-4'></div>
-            </div>
-            <FormErrorMessage>{errors.phoneNumber?.message}</FormErrorMessage>
+              <FormErrorMessage>{errors.phoneNumber?.message}</FormErrorMessage>
 
-            <div>
-              <Button>Send</Button>
-            </div>
-          </form>
+              <div>
+                <Button>Send</Button>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
+      )}
       {show && (
         <div className='min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
           <form onSubmit={handleSubmit(ValidateOtp)}>
             <div className='max-w-md w-full space-y-8'>
-              <TextInputField type='text' placeholder={'Enter your OTP'} />
+              <TextInputField type='text' placeholder={'Enter your OTP'} register={register('otp')} />
               <Button>Verify</Button>
             </div>
           </form>
--- src/Interface/numberVerification.interface.ts
+++ src/Interface/numberVerification.interface.ts
@@ -1,4 +1,4 @@
 export interface NumberVerificationLink {
- phoneNumber: any;
+ phoneNumber?: any;
  otp?: any;
 }
